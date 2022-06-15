fff625444e14074467c8547c8c839cc5cb327555
fernandojose95 <josefernandohernandezpaz@gmail.com>
2022-06-14 22:00:45 -0600

    refact & feat: implementando docker y levantando backend en docker

--- backend/githistory_api/githistory_api/asgi.py
+++ backend/githistory_api/githistory_api/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for history_api project.
+ASGI config for githistory_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'history_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'githistory_api.settings')
 
 application = get_asgi_application()
